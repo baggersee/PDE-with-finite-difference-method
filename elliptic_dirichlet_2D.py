161f3fb032543937bba590575c5c2d1ac58da3c6
baggersee <alpese73@gmail.com>
2022-08-12 13:44:53 +0200

    corrected small bug

--- elliptic_dirichlet_2D.py
+++ elliptic_dirichlet_2D.py
@@ -14,15 +14,15 @@ y_bottom = 0
 y_top = 1
 
 # Discretization data
-m = 12 # number of nodes in the x-axis
-n = 12 # number of nodes in the y-axis
+m = 10 # number of nodes in the x-axis
+n = 10 # number of nodes in the y-axis
 
 h = (x_right - x_left)/(m-1) # space for the x-axis discretization
 k = (y_top - y_bottom)/(n-1) # space for the y-axis discretization
 t = -2*(1/(h*h) + 1/(k*k))
 
 X = np.array([ x_left + t*h for t in range(m)])
-Y = np.array([ y_bottom + t*k for t in range(m)])
+Y = np.array([ y_bottom + t*k for t in range(n)])
 
 # Matrix calculation
 A = np.diag([1]*m*n)
@@ -48,7 +48,7 @@ b = np.concatenate((b,np.array([gx_yTop(X[j]) for j in range(m)])))
 U = np.matmul(A_,b)
 U = U.reshape(m,n,order='F').copy()
 U = U.transpose()
-X, Y = np.meshgrid(X, Y)
+X,Y = np.meshgrid(X,Y)
 
 # Plotting 3D
 
